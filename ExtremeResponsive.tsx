51f544bf10dfe4438cb758cb49eaf12a58722b54
NOBOIKE <53275945+noboike@users.noreply.github.com>
2024-04-09 01:47:04 +0300

    Update ExtremeResponsive.tsx
    fixes for next js

--- ExtremeResponsive.tsx
+++ ExtremeResponsive.tsx
@@ -1,4 +1,5 @@
-import { useLayoutEffect, useRef, useState } from "react";
+"use client";
+import { useLayoutEffect, useMemo, useRef, useState } from "react";
 
 interface ExtremeResponsiveProps {
   children: React.ReactNode;
@@ -11,15 +12,25 @@ export function ExtremeResponsive({
 }: ExtremeResponsiveProps) {
   const devRef = useRef(null);
   const [scaleRatio, setScaleRatio] = useState(1);
-  function handleLayOut() {
-    const observer = new ResizeObserver(() => {
-      let ratio = innerWidth / minWidth;
-      ratio = ratio > 1 ? 1 : ratio;
-      setScaleRatio(ratio);
-    });
-    if (devRef.current) observer.observe(devRef.current);
-  }
-  useLayoutEffect(handleLayOut, []);
+  const [isHidden, setIsHidden] = useState(true);
+  const handleLayOut = useMemo(() => {
+    return () => {
+      setTimeout(() => {
+        setIsHidden(false);
+      }, 0);
+      function scale() {
+        let ratio = innerWidth / minWidth;
+        ratio = ratio > 1 ? 1 : ratio;
+        setScaleRatio(ratio);
+      }
+      const observer = new ResizeObserver(() => {
+        scale();
+      });
+      scale();
+      if (devRef.current) observer.observe(devRef.current);
+    };
+  }, [minWidth]);
+  useLayoutEffect(handleLayOut, [minWidth]);
   return (
     <div
       ref={devRef}
@@ -27,9 +38,10 @@ export function ExtremeResponsive({
         transform: `translate(-50%, -50%) scale(${scaleRatio})`,
         width: `${100 * (scaleRatio < 1 ? minWidth / innerWidth : 1)}%`,
         height: `${100 * (scaleRatio < 1 ? minWidth / innerWidth : 1)}%`,
-        position: "absolute",
+        position: "relative",
         left: "50%",
         top: "50%",
+        transition: "0s",
       }}
     >
       {children}
